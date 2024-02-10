2c9fda70a2dfa10e530bd08b2c9c67cdbfdca9aa
ceyhun <ceyhunerturk1@gmail.com>
2024-02-10 22:56:14 +0300

    initial type traits impl

---
+++ erturk/meta/TypeTrait.hpp
@@ -0,0 +1,23 @@
+#ifndef ERTURK_META_H
+#define ERTURK_META_H
+
+#include <cstddef>
+
+namespace erturk
+{
+namespace detail
+{
+
+
+}  // namespace detail
+
+// ************************************************************************************************************************************
+
+namespace meta
+{
+
+}  // namespace meta
+
+}  // namespace erturk
+
+#endif  // ERTURK_META_H
