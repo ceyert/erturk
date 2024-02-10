c27e59e89fa67716944212543cb17b936bef95e0
ceyhun <ceyhunerturk1@gmail.com>
2024-02-11 00:31:12 +0300

    initial type computational impl

---
+++ erturk/meta/Computational.hpp
@@ -0,0 +1,25 @@
+#ifndef ERTURK_COMPUTATIONAL_H
+#define ERTURK_COMPUTATIONAL_H
+
+#include <cstddef>
+
+namespace erturk
+{
+namespace computational
+{
+
+namespace detail
+{
+
+
+
+}  // namespace detail
+
+// ************************************************************************************************************
+
+
+
+}  // namespace computational
+}  // namespace erturk
+
+#endif  // ERTURK_COMPUTATIONAL_H
