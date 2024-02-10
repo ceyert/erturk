3373b9eb4669f1952aed2792b8487e7967327f41
ceyhun <ceyhunerturk1@gmail.com>
2024-02-11 00:35:37 +0300

    initial type computational impl

--- erturk/meta/Computational.hpp
+++ erturk/meta/Computational.hpp
@@ -30,6 +30,23 @@ struct fibonacci_impl
 };
 
 
+template <unsigned int N>
+struct fibonacci_impl
+{
+    static const constexpr unsigned int value = fibonacci_impl<N - 1>::value + fibonacci_impl<N - 2>::value;
+};
+
+template <>
+struct fibonacci_impl<0>
+{  // Specialization for N = 0
+    static const constexpr unsigned int value = 0;
+};
+
+template <>
+struct fibonacci_impl<1>
+{  // Specialization for N = 1
+    static const constexpr unsigned int value = 1;
+};
 
 }  // namespace detail
 
@@ -41,6 +58,10 @@ struct factorial : detail::factorial_impl<N>
 };
 
 
+template <unsigned int N>
+struct fibonacci : detail::fibonacci_impl<N>
+{
+};
 
 }  // namespace computational
 }  // namespace erturk
