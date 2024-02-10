bd6cf2cdfd9606f4fe75d59feb9e32e67403f1a0
ceyhun <ceyhunerturk1@gmail.com>
2024-02-11 00:32:04 +0300

    initial type computational impl

--- erturk/meta/Computational.hpp
+++ erturk/meta/Computational.hpp
@@ -11,11 +11,34 @@ namespace computational
 namespace detail
 {
 
+template <unsigned int N>
+struct factorial_impl
+{
+    static const constexpr unsigned int value = N * factorial_impl<N - 1>::value;
+};
+
+template <>
+struct factorial_impl<0>
+{  // Base case specialization
+    static const constexpr unsigned int value = 1;
+};
+
+template <unsigned int N>
+struct fibonacci_impl
+{
+    static const constexpr unsigned int value = fibonacci_impl<N - 1>::value + fibonacci_impl<N - 2>::value;
+};
+
 
 
 }  // namespace detail
 
 // ************************************************************************************************************
+
+template <unsigned int N>
+struct factorial : detail::factorial_impl<N>
+{
+};
 
 
 
