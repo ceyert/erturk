5a415c16fa4c01b6a0af509a6a94dab676fd03b5
blldce <blldce@gmail.com>
2024-04-26 18:43:27 +0300

    SystemAllocator base impl added

---
+++ erturk/allocator/SystemAllocator.hpp
@@ -0,0 +1,91 @@
+#ifndef ERTURK_BITARRAY_H
+#define ERTURK_BITARRAY_H
+
+#include <new>
+#include <memory>
+
+namespace erturk
+{
+namespace allocator
+{
+
+template<class T>
+class SystemAllocator final {
+public:
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+    typedef T *pointer;
+    typedef const T *const_pointer;
+    typedef T &reference;
+    typedef const T &const_reference;
+    typedef T value_type;
+
+public:
+    SystemAllocator() noexcept = default;
+
+    SystemAllocator(const SystemAllocator &) noexcept = default;
+
+    SystemAllocator &operator=(const SystemAllocator &) noexcept = default;
+
+    SystemAllocator(SystemAllocator &&) noexcept = default;
+
+    SystemAllocator &operator=(SystemAllocator &&) noexcept = default;
+
+    ~SystemAllocator() noexcept = default;
+
+    T *allocate(std::size_t requestInBytes, std::size_t alignment = alignof(T)) {
+        if (requestInBytes == 0) {
+            return nullptr;
+        }
+        std::size_t required_size = requestInBytes * sizeof(T);
+
+        std::size_t total_size = required_size + alignment - 1 + sizeof(void *);
+
+        void *raw_memory = ::operator new(total_size);
+
+        void *start = static_cast<char *>(raw_memory) + sizeof(void *);
+
+        void *aligned_memory = std::align(alignment, required_size, start, total_size);
+
+        if (aligned_memory == nullptr) {
+            ::operator delete(raw_memory);
+            throw std::bad_alloc();
+        }
+
+        // Store the original pointer before the aligned address for proper deallocation
+        *((void **) ((char *) aligned_memory - sizeof(void *))) = raw_memory;
+        return static_cast<T *>(aligned_memory);
+    }
+
+    void deallocate(T *ptr, std::size_t n) noexcept {
+        if (ptr != nullptr) {
+            // Retrieve the original pointer stored just before the aligned address
+            void *raw_memory = *((void **) ((char *) ptr - sizeof(void *)));
+            ::operator delete(raw_memory);
+        }
+    }
+
+    template<typename U>
+    struct rebind {
+        using other = SystemAllocator<U>;
+    };
+
+    template<typename U>
+    void construct(U *ptr, U &&value) {
+        if (ptr != nullptr) {
+            new(ptr) U{std::forward<U>(value)};
+        }
+    }
+
+    template<typename U>
+    void destroy(U *ptr) {
+        if (ptr != nullptr) {
+            ptr->~U();
+        }
+    }
+};
+
+}  // namespace allocator
+}  // namespace erturk
+
+#endif  // ERTURK_BITARRAY_H
---
+++ samples/allocator/main.cpp
@@ -0,0 +1,54 @@
+
+#include "../../erturk/allocator/SystemAllocator.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+
+
+int main() {
+
+    {
+        std::vector<int, erturk::allocator::SystemAllocator<int>> my_vector(50, 42);
+
+        for (int i = 0; i < 1000; i++) {
+            my_vector.push_back(i);
+        }
+
+        for (const int& value: my_vector) {
+            std::cout << "Value: " << value << " at Address: " << &value << std::endl;
+        }
+    }
+
+    {
+        std::list<int, erturk::allocator::SystemAllocator<int>> my_list{};
+
+        for (int i = 0; i < 500; i++) {
+            my_list.push_back(i);
+        }
+
+        for (int i = 0; i < 500; i++) {
+            my_list.push_front(i);
+        }
+
+        for (const int& value: my_list) {
+            std::cout << "Value: " << value << " at Address: " << &value << std::endl;
+        }
+    }
+
+
+    {
+        std::map<const int, std::string, std::less<int>, erturk::allocator::SystemAllocator<std::pair<const int, std::string>>> my_map{};
+
+        for (int i = 0; i < 1000; i++) {
+            my_map.emplace(i, std::to_string(i));
+        }
+
+        for (const auto &pair: my_map) {
+            std::cout << "Key: " << pair.first << " Value: " << pair.second << " at Address: " << &pair << std::endl;
+        }
+
+    }
+
+    return 0;
+}
