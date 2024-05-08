db30fee28843fc40bc9137dba2910eb15f4d13a9
ceyhun <ceyhunerturk1@gmail.com>
2024-05-08 20:20:37 +0300

    system allocator refined

--- samples/allocator/main.cpp
+++ samples/allocator/main.cpp
@@ -1,20 +1,21 @@
-
 #include "../../erturk/allocator/AlignedSystemAllocator.hpp"
 #include <iostream>
-#include <vector>
 #include <list>
 #include <map>
+#include <vector>
 
-int main() {
-
+int main()
+{
     {
         std::vector<int, erturk::allocator::AlignedSystemAllocator<int>> my_vector(50, 42);
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1000; i++)
+        {
             my_vector.push_back(i);
         }
 
-        for (const int& value: my_vector) {
+        for (const int& value : my_vector)
+        {
             std::cout << "Value: " << value << " at Address: " << &value << std::endl;
         }
     }
@@ -22,31 +23,36 @@ int main() {
     {
         std::list<int, erturk::allocator::AlignedSystemAllocator<int>> my_list{};
 
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 500; i++)
+        {
             my_list.push_back(i);
         }
 
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 500; i++)
+        {
             my_list.push_front(i);
         }
 
-        for (const int& value: my_list) {
+        for (const int& value : my_list)
+        {
             std::cout << "Value: " << value << " at Address: " << &value << std::endl;
         }
     }
 
-
     {
-        std::map<const int, std::string, std::less<int>, erturk::allocator::AlignedSystemAllocator<std::pair<const int, std::string>>> my_map{};
+        std::map<const int, std::string, std::less<int>,
+                 erturk::allocator::AlignedSystemAllocator<std::pair<const int, std::string>>>
+            my_map{};
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1000; i++)
+        {
             my_map.emplace(i, std::to_string(i));
         }
 
-        for (const auto &pair: my_map) {
+        for (const auto& pair : my_map)
+        {
             std::cout << "Key: " << pair.first << " Value: " << pair.second << " at Address: " << &pair << std::endl;
         }
-
     }
 
     return 0;
